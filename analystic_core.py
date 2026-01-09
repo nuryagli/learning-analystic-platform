719f4e0e9945a25f963b5a47233a61b74482127d
Nurbeniz <102666235+nuryagli@users.noreply.github.com>
2026-01-09 14:41:16 +0300

    Add core analystic file

---
+++ analystic_core.py
@@ -0,0 +1,19 @@
+# Bu dosya egitim verilerinden ogrenme metrikleri cikarmak icin var
+
+def calculate_average_score(quiz_results):
+    """
+    Amac: Ogrencilerin genel basari seviyesini olcmek
+    """
+    pass 
+
+def find_topic_weakness(quiz_results):
+    """
+    Amac: Konu bazli zayiflik analizi yapmak
+    """
+    pass 
+
+def calculate_progress_trend(quiz_results):
+    """
+    Amac: Zaman icinde ogrenme ilerlemesini analiz etmek
+    """
+    pass 
