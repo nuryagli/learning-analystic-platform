f8e917c5c0f071b8b6bbabed970c0875933ad450
Nurbeniz <102666235+nuryagli@users.noreply.github.com>
2026-01-16 23:56:08 +0300

    feat: find_topic_weakness fonksiyonu eklendi - konu bazli zayiflik analizi implementasyonu

--- analystic_core.py
+++ analystic_core.py
@@ -35,11 +35,39 @@ print(calculate_average_score(quiz_results))
 def find_topic_weakness(quiz_results):
     """
     Amac: Konu bazli zayiflik analizi yapmak
+    Tanim:
+    Konu ortalamasi ne kadar dusuk ise o konuda zayiflik var.
+
+    Donen deger: 
+    [("topic", average_score), ......] en zayiftan en gucluye sirali liste
     """
-    pass 
+    topic_scores = {}
+
+    #Topic bazinda skorlari grupla 
+
+    for result in quiz_results:
+        topic = result["topic"]
+        score = result["score"]
+
+        if topic not in topic_scores:
+            topic_scores[topic] = []
+        topic_scores[topic].append(score)
+
+    # Topic ortalamalarini hespala
+    topic_averages = []
+    for topic, scores in topic_scores.items():
+        avg = sum(scores) / len(scores)
+        topic_averages.append((topic, avg))
+
+    # En zayiftan en gucluya sirala
+    topic_averages.sort(key=lambda x: x[1])
+
+    return topic_averages
+
 
 def calculate_progress_trend(quiz_results):
     """
     Amac: Zaman icinde ogrenme ilerlemesini analiz etmek
     """
     pass 
+print(find_topic_weakness(quiz_results))
